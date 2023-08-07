fad6312eb1fc93227b6aba85513d69d4272f3940
Tom Tobar <cocdetombomb@gmail.com>
2023-08-07 01:34:34 -0500

    add o4 branch

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 - [x] Create a Virtual Env with `pipenv` and install dependencies
 - [x] Initialize migrations with `alembic`
 - [x] Configure `alembic` to work with our DB
-- [ ] Create a schema and migrate the DB
+- [x] Create a schema and migrate the DB
 - [ ] Use migrations to:
   - create/drop tables
   - rename tables
@@ -47,3 +47,45 @@
 - Code demo
   - Creating a schema
   - Generate and run migrations
+
+
+## Evolving The DB Through Migrations
+
+Create the following migrations:
+```python
+# Migration 1: Change user table to users
+
+# Migration 2: Add email column
+# Add a new column called "email" to the "user" table.
+
+# Migration 3: Add unique constraint on username
+# Add a unique constraint on the "username" column in the "user" table.
+
+# - error
+# - solution - https://stackoverflow.com/questions/30378233/sqlite-lack-of-alter-support-alembic-migration-failing-because-of-this-solutio
+    # - add render_as_batch=True to the env.py file 
+    # downgrade the DB to base
+    # redo all migrations
+    # test with seeds
+
+# Migration 4: Add created_at and updated_at timestamps
+# Add "created_at" and "updated_at" timestamp columns to the "user" table to track creation and update times.
+
+# func - https://docs.sqlalchemy.org/en/14/core/functions.html#selected-known-functions
+# server_default - https://docs.sqlalchemy.org/en/14/orm/persistence_techniques.html#fetching-server-generated-defaults
+# onupdate - https://docs.sqlalchemy.org/en/14/core/constraints.html#on-update-and-on-delete
+
+# Test updated_at in seeds file
+# __repr__ method:
+    # def __repr__(self):
+    #     return f"\n<User "\
+    #         + f"id={self.id}," \
+    #         + f"username={self.username}," \
+    #         + f"email={self.email}," \
+    #         + f"created_at={self.create_at}," \
+    #         + f"updated_at={self.updated_at}," \
+
+
+# Migration 5: Add phone number column
+# Add a new column called "phone_number" to the "user" table to store user contact phone numbers.
+```
--- models.py
+++ models.py
@@ -1,17 +1,47 @@
-# We will need to import:
-#   - Column, Integer, and String from sqlalchemy
-#   - declarative_base from sqlalchemy.orm https://docs.sqlalchemy.org/en/14/orm/mapping_api.html#sqlalchemy.orm.declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
     
-# Define the Base class
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    
 
-# Define a User class and inherit from Base
+# Migration 1: Change user table to users
 
-# Define the table name using __tablename__
+# Migration 2: Add email column
+# Add a new column called "email" to the "user" table.
 
-# Add columns, id and username
+# Migration 3: Add unique constraint on username
+# Add a unique constraint on the "username" column in the "user" table.
 
-# generate migrations with alembic
+# - error
+# - solution - https://stackoverflow.com/questions/30378233/sqlite-lack-of-alter-support-alembic-migration-failing-because-of-this-solutio
+    # - add render_as_batch=True to the env.py file 
+    # downgrade the DB to base
+    # redo all migrations
+    # test with seeds
 
-# Run the migration and check DB
+# Migration 4: Add created_at and updated_at timestamps
+# Add "created_at" and "updated_at" timestamp columns to the "user" table to track creation and update times.
 
-# Populate DB by running seed file
+# func - https://docs.sqlalchemy.org/en/14/core/functions.html#selected-known-functions
+# server_default - https://docs.sqlalchemy.org/en/14/orm/persistence_techniques.html#fetching-server-generated-defaults
+# onupdate - https://docs.sqlalchemy.org/en/14/core/constraints.html#on-update-and-on-delete
+
+# Test updated_at in seeds file
+# __repr__ method:
+    # def __repr__(self):
+    #     return f"\n<User "\
+    #         + f"id={self.id}," \
+    #         + f"username={self.username}," \
+    #         + f"email={self.email}," \
+    #         + f"created_at={self.create_at}," \
+    #         + f"updated_at={self.updated_at}," \
+
+
+# Migration 5: Add phone number column
+# Add a new column called "phone_number" to the "user" table to store user contact phone numbers.
